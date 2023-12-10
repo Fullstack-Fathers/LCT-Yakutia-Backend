90bd365f365e3594bb509ac61ba4dc4d08174e0d
Amantaev-Artur <amantaev_artur@mail.ru>
2023-12-10 20:53:08 +0300

    vk

--- app.js
+++ app.js
@@ -1,26 +1,63 @@
 const express = require('express')
 const axios = require('axios');
 require('dotenv').config();
+const professionProbabilities = require('./config/professions_vk')
 
 const app = express()
 const port = 3000
 
 app.use(express.json());
 
+function recommendProfessions(userSubscriptions) {
+  const professionScores = {};
+
+  // Подсчет вероятностей для каждой профессии
+  userSubscriptions.forEach(category => {
+    const professions = professionProbabilities[category];
+    if (professions) {
+      Object.entries(professions).forEach(([profession, probability]) => {
+        professionScores[profession] = (professionScores[profession] || 0) + probability;
+      });
+    }
+  });
+
+  // Сортировка профессий по убыванию вероятностей
+  const sortedProfessions = Object.entries(professionScores)
+    .sort((a, b) => b[1] - a[1])
+    .map(([profession, probability]) => ({ profession, probability }));
+
+  return sortedProfessions;
+}
+
+
 app.get('/social_networks/auth', async (req, res) => {
   try {
-  const { code } = req.body;
+    const { code, social_network } = req.body;
 
-  const clientId = process.env.CLIENT_ID;
-  const clientSecret = process.env.CLIENT_SECRET;
+    if (social_network == 'vk') {
+      const clientId = process.env.CLIENT_ID;
+      const clientSecret = process.env.CLIENT_SECRET;
 
-  const vkTokenUrl = `https://oauth.vk.com/access_token?client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=http://mysite.ru&code=${code}`;
+      const vkTokenUrl = `https://oauth.vk.com/access_token?client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=http://mysite.ru&code=${code}`;
 
-  const response = await axios.get(vkTokenUrl);
+      const response = await axios.get(vkTokenUrl);
 
-  const { access_token } = response.data
-  // тут должна быть логика получения групп и их анализ
-  res.send({ status: 'ok' })
+      const accessToken = response.data.access_token;
+
+      const vkGroupsUrl = 'https://api.vk.com/method/groups.get';
+      const groupsResponse = await axios.post(vkGroupsUrl, {
+        extended: 1,
+        access_token: accessToken,
+        v: '5.199',
+      });
+
+      const groupsData = groupsResponse.data;
+      const activitiesArray = groupsData.response.items.map(item => item.activity);
+
+      const result = recommendProfessions(activitiesArray);
+
+      res.send(result)
+    }
   } catch (error) {
     res.status(500).send('Internal Server Error');
   }
