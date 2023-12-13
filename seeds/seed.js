de2d677d2cf2dae8329ba9e184c1b66fb6870da8
Амантаев Артур <aa@megaperfume.ru>
2023-12-14 00:13:39 +0300

    seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -12,9 +12,6 @@ exports.seed = async function(knex) {
     {id: 1, title: 'Повар', description: 'Готовьте изысканные блюда и получайте за это деньги. Разве это не супер?)'}
   ]);
   await knex('prof_recommendations').del()
-  await knex('prof_recommendations').insert([
-    {id: 1, user_id: 1, profession_id: 1, percent: 0.7, is_watch: false}
-  ]);
   await knex('courses').del()
   await knex('courses').insert([
     {id: 1, title: 'Жарка и варка от шеф повара', description: 'Научитесь очень многому', image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgbWEuxLS3myKtOsrn-Kf5G0ogVrCgl5XbCvTOrkDhGg&s'}
