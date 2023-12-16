897756dd48ae0e58ca6dee323ffb8cd6cddacd0f
elizavetakrasova <91592797+elizavetakrasova@users.noreply.github.com>
2023-12-16 17:02:19 +0700

    added auth (#2)
    * added auth
    
    * added method
    
    * added methods
    
    * added
    
    * fix
    
    ---------
    
    Co-authored-by: e.a.krasova <e.a.krasova>

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const axios = require('axios');
 require('dotenv').config();
 const professionProbabilities = require('./config/professions_vk')
@@ -19,6 +22,7 @@ const client = new Client({
 client.connect();
 
 app.use(express.json());
+app.use(cors());
 
 function recommendProfessions(userSubscriptions) {
   const professionScores = {};
@@ -41,6 +45,17 @@ function recommendProfessions(userSubscriptions) {
   return sortedProfessions;
 }
 
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'] 
+  const token = authHeader && authHeader.split(' ')[1] 
+  if (!token) return res.sendStatus(401)
+  jwt.verify(token, 'secret123', (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+    })
+  }
 
 app.post('/social_networks/auth', async (req, res) => {
   try {
@@ -139,13 +154,53 @@ app.post('/social_networks/auth', async (req, res) => {
   }
 })
 
+app.get('/professions', authenticateToken, async (req, res) => {
+  const result = await client.query('SELECT * FROM professions');
+  res.send(result.rows)
+})
 
-app.get('/courses', async (req, res) => {
+app.get('/professions/:id', authenticateToken, async (req, res) => {
+  const courseId = req.params.id;
+  const result = await client.query('SELECT * FROM professions WHERE id = $1', [courseId]);
+  if (result.rows.length > 0) {
+    res.send(result.rows[0]);
+  } else {
+    res.status(404).send('Professions not found');
+  }})
+
+app.post('/profession', authenticateToken, async (req, res) => {
+  try {
+    const professionFind = await client.query('SELECT * FROM professions WHERE title = $1', [req.body.title]);
+
+    if (!professionFind.rows[0]) {
+      const insertQuery = {
+        text: 'INSERT INTO professions(title, description) VALUES($1, $2)',
+        values: [req.body.title, req.body.description],
+      };
+      await client.query(insertQuery);
+      const profession = await client.query('SELECT * FROM professions WHERE title = $1', [req.body.title]);
+    
+      return res.json(profession.rows[0]);
+  
+    } else {
+      return res.status(400).json({
+        error: 'Такая профессия уже существует',
+      });
+    }
+
+  } catch (err) {
+    res.status(500).json({
+      error: 'Не удалось создать курс',
+    });
+  }
+})
+
+app.get('/courses', authenticateToken, async (req, res) => {
   const result = await client.query('SELECT * FROM courses');
   res.send(result.rows)
 })
 
-app.get('/courses/:id', async (req, res) => {
+app.get('/courses/:id', authenticateToken, async (req, res) => {
   const courseId = req.params.id;
   const result = await client.query('SELECT * FROM courses WHERE id = $1', [courseId]);
   if (result.rows.length > 0) {
@@ -153,6 +208,207 @@ app.get('/courses/:id', async (req, res) => {
   } else {
     res.status(404).send('Course not found');
   }})
+
+app.post('/course', authenticateToken, async (req, res) => {
+  try {
+    const courseFind = await client.query('SELECT * FROM courses WHERE title = $1', [req.body.title]);
+
+    if (!courseFind.rows[0]) {
+      const insertQuery = {
+        text: 'INSERT INTO courses(title, description, image_url) VALUES($1, $2, $3)',
+        values: [req.body.title, req.body.description, req.body.image_url],
+      };
+      await client.query(insertQuery);
+      const course = await client.query('SELECT * FROM courses WHERE title = $1', [req.body.title]);
+    
+      return res.json(course.rows[0]);
+  
+    } else {
+      return res.status(400).json({
+        error: 'Такой курс уже существует',
+      });
+    }
+
+  } catch (err) {
+    res.status(500).json({
+      error: 'Не удалось создать курс',
+    });
+  }
+})
+
+app.post('/auth/login', async (req, res) => {
+  try {
+    const emailReq = req.body.email;
+    const user = await client.query('SELECT * FROM users WHERE email = $1 limit 1', [emailReq]);
+
+    if (!user.rows[0]) {
+      return res.status(404).json({
+        error: 'Пользователь не найден',
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(req.body.password, user.rows[0].password_hash);
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        error: 'Неверный логин или пароль',
+      });
+    }
+
+    const token = jwt.sign(
+        {
+          _id: user.rows[0].id,
+        },
+        'secret123',
+        {
+          expiresIn: '30d',
+        },
+    );
+
+    const {passwordHash, ...userData} = user.rows[0];
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: 'Не удалось авторизоваться',
+    });
+  }
+})
+
+app.get('/user/:id', authenticateToken, async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await client.query('SELECT * FROM users WHERE id = $1 limit 1', [userId]);
+
+    if (!user.rows[0]) {
+      return res.status(404).json({
+        error: 'Пользователь не найден',
+      });
+    }
+
+    return res.json(user.rows[0]);
+  } catch (err) {
+    res.status(500).json({
+      error: 'Не удалось выполнить запрос',
+    });
+  }
+})
+
+app.get('/user_rec_profession/:id', authenticateToken, async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user_rec_prof = await client.query(
+      'SELECT * FROM prof_recommendations left join professions on professions.id = prof_recommendations.profession_id WHERE user_id = $1',
+      [userId]);
+
+    if (!user_rec_prof.rows[0]) {
+      return res.status(404).json({
+        error: 'У пользователя нет рекомендованных курсов',
+      });
+    }
+
+    return res.json(user_rec_prof.rows[0]);
+  } catch (err) {
+    res.status(500).json({
+      error: 'Не удалось выполнить запрос',
+    });
+  }
+})
+
+app.get('/courses_for_prof/:prof_id', authenticateToken, async (req, res) => {
+  try {
+    const profId = req.params.prof_id;
+    const prof_courses = await client.query(
+      'SELECT * FROM prof_courses left join courses on courses.id = prof_courses.course_id WHERE profession_id = $1', 
+    [profId]);
+
+    if (!prof_courses.rows[0]) {
+      return res.status(404).json({
+        error: 'У профессии нет курсов',
+      });
+    }
+
+    return res.json(prof_courses.rows[0]);
+  } catch (err) {
+    res.status(500).json({
+      error: 'Не удалось выполнить запрос',
+    });
+  }
+})
+
+app.get('/prof_for_courses_/:course_id', authenticateToken, async (req, res) => {
+  try {
+    const courseId = req.params.course_id;
+    const prof_courses = await client.query(
+      'SELECT * FROM prof_courses left join professions on professions.id = prof_courses.profession_id WHERE course_id = $1', 
+    [courseId]);
+
+    if (!prof_courses.rows[0]) {
+      return res.status(404).json({
+        error: 'У курса нет профессий',
+      });
+    }
+
+    return res.json(prof_courses.rows[0]);
+  } catch (err) {
+    res.status(500).json({
+      error: 'Не удалось выполнить запрос',
+    });
+  }
+})
+
+app.post('/auth/register', async (req, res) => {
+  try {
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    
+    const userFind = await client.query('SELECT * FROM users WHERE email = $1 limit 1', [req.body.email]);
+
+    if (!userFind.rows[0]) {
+      const insertQuery = {
+        text: 'INSERT INTO users(firstname, lastname, email, password_hash, role) VALUES($1, $2, $3, $4, $5)',
+        values: [req.body.firstname, req.body.lastname, req.body.email, hash, req.body.role],
+      };
+      await client.query(insertQuery);
+  
+      const emailReq = req.body.email;
+      const user = await client.query('SELECT * FROM users WHERE email = $1', [req.body.email]);
+  
+      const token = jwt.sign(
+          {
+            _id: user.rows[0].id,
+          },
+          'secret123',
+          {
+            expiresIn: '30d',
+          },
+      );
+  
+      const {passwordHash, ...userData} = user.rows[0];
+  
+      res.json({
+        ...userData,
+        token,
+      });
+  
+    } else {
+      return res.status(400).json({
+        error: 'Пользователь с такой почтой уже существует',
+      });
+    }
+
+  } catch (err) {
+    res.status(500).json({
+      error: 'Не удалось зарегистрироваться',
+    });
+  }
+})
+
+
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening on port ${port}`)
